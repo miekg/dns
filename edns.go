f16ad2a7afdc88946d850b70897e5f698c881521
Miek Gieben <miek@miek.nl>
2010-12-23 14:27:04 +0100

    Add some helper function for ENDS
    cleanups - starts to work. Still FORMERR on EDNS0 request, but
    we are getting there

--- edns.go
+++ edns.go
@@ -16,15 +16,16 @@ type Option struct {
 
 // EDNS extended RR.
 // Not used yet
-type EDNS0_Header struct {
-	Name          string "extended-name"
+/* 
+This is the EDNS0 Header
+	Name          string "domain-name"
 	Opt           uint16 // was type, but is always TypeOPT
 	UDPSize       uint16 // was class
 	ExtendedRcode uint8  // was TTL
 	Version       uint8  // was TTL
 	Z             uint16 // was TTL (all flags should be put here)
 	Rdlength      uint16 // length of data after the header
-}
+*/
 
 type RR_OPT struct {
 	Hdr    RR_Header // this must become a EDNS0_Header
@@ -36,7 +37,7 @@ func (rr *RR_OPT) Header() *RR_Header {
 }
 
 func (rr *RR_OPT) String() string {
-	s := rr.Hdr.String()
+	s := rr.Hdr.String() // Hier misschien andere representatie
 	for _, o := range rr.Option {
 		switch o.Code {
 		case OptionCodeNSID:
@@ -44,4 +45,19 @@ func (rr *RR_OPT) String() string {
 		}
 	}
 	return s
+}
+
+// when set is true, set the size otherwise get it
+func (rr *RR_OPT) UDPSize(size int, set bool) int {
+        return 0
+}
+
+// when set is true, set the Do bit, otherwise get it
+func (rr *RR_OPT) DoBit(do, set bool) bool {
+        return true
+}
+
+// when set is true, set the nsid, otherwise get it
+func (rr *RR_OPT) Nsid(nsid string, set bool) string {
+        return ""
 }
