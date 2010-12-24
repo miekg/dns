7d4a26deac2ef82b9edba147204aead989da30c0
Miek Gieben <miek@miek.nl>
2010-12-24 17:26:40 +0100

    chaos
    also ask for hostname.bind

--- examples/chaos.go
+++ examples/chaos.go
@@ -15,6 +15,7 @@ func main() {
         r.Servers = []string{"127.0.0.1"}
         r.Timeout = 2
         r.Attempts = 1
+        var in dns.DnsMsg
 
         if len(os.Args) != 2 {
                 fmt.Printf("%s NAMESERVER\n", os.Args[0])
@@ -28,7 +29,13 @@ func main() {
                 r.Servers = []string{a.String()}
                 m.Question[0] = dns.Question{"version.bind.", dns.TypeTXT, dns.ClassCHAOS}
                 qr <- dns.DnsMsg{m, nil}
-                in := <-qr
+                in = <-qr
+                if in.Dns != nil && in.Dns.Answer != nil {
+                        fmt.Printf("%v\n", in.Dns.Answer[0])
+                }
+                m.Question[0] = dns.Question{"hostname.bind.", dns.TypeTXT, dns.ClassCHAOS}
+                qr <- dns.DnsMsg{m, nil}
+                in = <-qr
                 if in.Dns != nil && in.Dns.Answer != nil {
                         fmt.Printf("%v\n", in.Dns.Answer[0])
                 }
