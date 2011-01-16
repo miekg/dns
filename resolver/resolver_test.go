30e535b495c1cfcb8b2ddfde5dd0e933b1ffae14
Miek Gieben <miek@miek.nl>
2011-01-16 20:10:29 +0100

    Fold testcase in 1 file

--- resolver/resolver_test.go
+++ resolver/resolver_test.go
@@ -128,3 +128,20 @@ func TestResolverTsig(t *testing.T) {
 	ch <- Msg{nil, nil}
 	<-ch // wait for ch to close channel
 }
+
+func TestAXFR(t *testing.T) {
+	res := new(Resolver)
+	ch := res.NewXfer()
+
+	res.Servers = []string{"127.0.0.1"}
+	m := new(dns.Msg)
+	m.Question = make([]dns.Question, 1)
+        m.Question[0] = dns.Question{"miek.nl", dns.TypeAXFR, dns.ClassINET}
+	//m.Question[0] = dns.Question{"atoom.net", dns.TypeAXFR, dns.ClassINET}
+
+        ch <- Msg{m, nil}
+	for dm := range ch {
+                var _ = dm
+                /* fmt.Printf("%v\n",dm.Dns) */
+        }
+}
