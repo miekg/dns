e544237d3692bed3b152a5c84a78a8fdeec5c667
Miek Gieben <miek@miek.nl>
2011-01-18 19:19:42 +0100

    slightly better formatting of the flags in msg.String()

--- msg.go
+++ msg.go
@@ -698,30 +698,30 @@ func (h *MsgHdr) String() string {
 	s += ", status: " + rcode_str[h.Rcode]
 	s += ", id: " + strconv.Itoa(int(h.Id)) + "\n"
 
-	s += ";; flags: "
+	s += ";; flags:"
 	if h.Response {
-		s += "qr "
+		s += " qr"
 	}
 	if h.Authoritative {
-		s += "aa "
+		s += " aa"
 	}
 	if h.Truncated {
-		s += "tc "
+		s += " tc"
 	}
 	if h.RecursionDesired {
-		s += "rd "
+		s += " rd"
 	}
 	if h.RecursionAvailable {
-		s += "ra "
+		s += " ra"
 	}
 	if h.Zero { // Hmm
-		s += "z "
+		s += " z"
 	}
 	if h.AuthenticatedData {
-		s += "ad "
+		s += " ad"
 	}
 	if h.CheckingDisabled {
-		s += "cd "
+		s += " cd"
 	}
 
 	s += ";"
