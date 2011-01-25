db6b13c411e53b58072aba7a297add036ffddd41
Miek Gieben <miek@miek.nl>
2011-01-25 12:46:10 +0100

    fix doc

--- _examples/funkensturm/doc.go
+++ _examples/funkensturm/doc.go
@@ -1,19 +1,16 @@
 /*
 Funkensturm rewrites DNS packets in the broadest sense of the word.
-The rewritting can include delayed (re)sending of packets, (re)sending
+The rewriting can include delayed (re)sending of packets, (re)sending
 packets to multiple servers, rewriting the packet contents, for instance
 by signing a packet, or the other way around, stripping the signatures.
 
 In its essence this is no different that a recursive nameserver, which also
 receives and sends queries. The difference is the huge amount of tweaking
-funkensturm offers.
+Funkensturm offers.
 
-It includes a configuration language which makes setting up funkensturm
-real easy. (It may be the case that this configuration language will be Go)
-
-Not sure if this is doable:
-- support packet of death (TSIG signed) for stopping funkensturm
-- support packet of config (TSIG signed) for configuring funkensturm on the fly
+The configuration of Funkensturm is done by writing it in Go - a
+separate configuration language was deemed to be unpractical and
+would limit the possibilities.
 
 */
 package documentation
