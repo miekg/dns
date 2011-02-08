d0e343e35e7bb00033cc0aea8b827c826acedc25
Miek Gieben <miek@miek.nl>
2011-02-08 20:18:47 +0100

    Fix axfr

--- _examples/axfr/axfr.go
+++ _examples/axfr/axfr.go
@@ -3,20 +3,20 @@ package main
 import (
         "fmt"
         "dns"
-        "dns/resolver"
 )
 
 func main() {
-	res := new(resolver.Resolver)
-	ch := res.NewXfer()
-
+	res := new(dns.Resolver)
 	res.FromFile("/etc/resolv.conf")
+
+        ch := make(chan *dns.Msg)
+
 	m := new(dns.Msg)
 	m.Question = make([]dns.Question, 1)
 	m.Question[0] = dns.Question{"atoom.net", dns.TypeAXFR, dns.ClassINET}
 
-        ch <- resolver.Msg{m, nil, nil}
-	for dm := range ch {
-                fmt.Printf("%v\n",dm.Dns)
+        go res.Axfr(m, ch)
+	for x := range ch {
+                fmt.Printf("%v\n",x)
         }
 }
