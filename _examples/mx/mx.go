7794ab9a5cfdd9df23803770279b0e102283e2c4
Miek Gieben <miek@miek.nl>
2011-02-09 22:10:35 +0100

    fix the mx code too

--- _examples/mx/mx.go
+++ _examples/mx/mx.go
@@ -4,14 +4,12 @@ package main
 // (c) Miek Gieben - 2011
 import (
 	"dns"
-        "dns/resolver"
         "os"
         "fmt"
 )
 
 func main() {
-	r := new(resolver.Resolver)
-	qr := r.NewQuerier()
+	r := new(dns.Resolver)
         r.FromFile("/etc/resolv.conf")
         if len(os.Args) != 2 {
                 fmt.Printf("%s DOMAIN\n", os.Args[0])
@@ -23,22 +21,17 @@ func main() {
         m.Question = make([]dns.Question, 1)
         m.Question[0] = dns.Question{os.Args[1], dns.TypeMX, dns.ClassINET}
 
-        qr <- resolver.Msg{m, nil, nil}
-        in := <-qr
-        if in.Dns != nil {
-                if in.Dns.Rcode != dns.RcodeSuccess {
+        in, err := r.Query(m)
+        if in != nil {
+                if in.Rcode != dns.RcodeSuccess {
                         fmt.Printf(" *** invalid answer name %s after MX query for %s\n", os.Args[1], os.Args[1])
                         os.Exit(1)
                 }
                 // Stuff must be in the answer section
-                for _, a := range in.Dns.Answer {
+                for _, a := range in.Answer {
                         fmt.Printf("%v\n", a)
                 }
         } else {
-                fmt.Printf("*** error: %s\n", in.Error.String())
+                fmt.Printf("*** error: %s\n", err.String())
         }
-
-        // Stop the resolver, send it a null mesg
-        qr <- resolver.Msg{nil, nil, nil}
-        <-qr
 }
