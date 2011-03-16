d728de708808114f085a8c5bf2775f2c53bed772
Miek Gieben <miek@miek.nl>
2011-03-16 17:28:35 +0100

    nicer api

--- defaults.go
+++ defaults.go
@@ -1,20 +1,10 @@
 package dns
 
-// Create a notify request packet.
-func (dns *Msg) SetNotifyRequest(z string, class uint16) {
+// Create a notify packet.
+func (dns *Msg) SetNotify(z string, class uint16) {
 	dns.MsgHdr.Opcode = OpcodeNotify
 	dns.MsgHdr.Authoritative = true
 	dns.MsgHdr.Id = Id()
-	dns.Question = make([]Question, 1)
-	dns.Question[0] = Question{z, TypeSOA, class}
-}
-
-// Create a notify reply packet.
-func (dns *Msg) SetNotifyReply(z string, class, id uint16) {
-	dns.MsgHdr.Opcode = OpcodeNotify
-	dns.MsgHdr.Authoritative = true
-	dns.MsgHdr.Response = true
-	dns.MsgHdr.Id = id
 	dns.Question = make([]Question, 1)
 	dns.Question[0] = Question{z, TypeSOA, class}
 }
@@ -30,7 +20,8 @@ func (dns *Msg) IsNotify() bool {
 	return ok
 }
 
-func (dns *Msg) SetIxfrRequest(z string, class uint16, serial uint32) {
+// Create a dns msg suitable for requesting an ixfr.
+func (dns *Msg) SetIxfr(z string, class uint16, serial uint32) {
 	dns.Question = make([]Question, 1)
 	dns.Ns = make([]RR, 1)
 	s := new(RR_SOA)
@@ -41,7 +32,8 @@ func (dns *Msg) SetIxfrRequest(z string, class uint16, serial uint32) {
         dns.Ns[0] = s
 }
 
-func (dns *Msg) SetAxfrRequest(z string, class uint16) {
+// Create a dns msg suitable for requesting an axfr.
+func (dns *Msg) SetAxfr(z string, class uint16) {
 	dns.Question = make([]Question, 1)
 	dns.Question[0] = Question{z, TypeAXFR, class}
 }
