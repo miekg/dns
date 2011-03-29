5325936b7be3887c80d14cdc28813a58d800e765
Miek Gieben <miek@miek.nl>
2011-03-29 10:56:43 +0200

    Fix chaos example

--- _examples/chaos/chaos.go
+++ _examples/chaos/chaos.go
@@ -7,12 +7,10 @@ import (
 	"dns"
 	"os"
 	"fmt"
-	"net"
 )
 
 func main() {
-	r := new(dns.Resolver)
-        r.FromFile("/etc/resolv.conf")
+	c, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
 	if len(os.Args) != 2 {
 		fmt.Printf("%s DOMAIN\n", os.Args[0])
 		os.Exit(1)
@@ -20,55 +18,58 @@ func main() {
 
 	m := new(dns.Msg)
 	m.Question = make([]dns.Question, 1)
-	for _, a := range addresses(r, os.Args[0]) {
-		// set the resolver to query the NS directly
-		r.Servers = []string{a.String()}
+	d := new(dns.Conn)
+	d.RemoteAddr = c.Servers[0]
+	d.Dial("udp")
+	for _, a := range addresses(d, os.Args[0]) {
+		d.RemoteAddr = a
+		d.Dial("udp") // Redial TODO(mg)
 		m.Question[0] = dns.Question{"version.bind.", dns.TypeTXT, dns.ClassCHAOS}
-                in, _ := r.Query(m)
+		in, _ := dns.QuerySimple(d, m)
 		if in != nil && in.Answer != nil {
 			fmt.Printf("%v\n", in.Answer[0])
 		}
 		m.Question[0] = dns.Question{"hostname.bind.", dns.TypeTXT, dns.ClassCHAOS}
-                in, _ = r.Query(m)
+		in, _ = dns.QuerySimple(d, m)
 		if in != nil && in.Answer != nil {
 			fmt.Printf("%v\n", in.Answer[0])
 		}
 	}
 }
 
-func addresses(r *dns.Resolver, name string) []net.IP {
+func addresses(d *dns.Conn, name string) []string {
 	m := new(dns.Msg)
 	m.MsgHdr.RecursionDesired = true //only set this bit
 	m.Question = make([]dns.Question, 1)
-	var ips []net.IP
+	var ips []string
 
 	m.Question[0] = dns.Question{os.Args[1], dns.TypeA, dns.ClassINET}
-        in, err := r.Query(m)
-        if in == nil {
-                fmt.Printf("Nothing recevied: %s\n", err.String())
-                return nil
-        }
+	in, err := dns.QuerySimple(d, m)
+	if in == nil {
+		fmt.Printf("Nothing recevied: %s\n", err.String())
+		return nil
+	}
 
 	if in.Rcode != dns.RcodeSuccess {
 		return nil
 	}
 	// Stuff must be in the answer section
 	for _, a := range in.Answer {
-		ips = append(ips, a.(*dns.RR_A).A)
+		ips = append(ips, a.(*dns.RR_A).A.String()+":53")
 	}
 	m.Question[0] = dns.Question{os.Args[1], dns.TypeAAAA, dns.ClassINET}
-        in, err = r.Query(m)
-        if in == nil {
-                fmt.Printf("Nothing recevied: %s\n", err.String())
-                return nil
-        }
+	in, err = dns.QuerySimple(d, m)
+	if in == nil {
+		fmt.Printf("Nothing recevied: %s\n", err.String())
+		return ips
+	}
 
 	if in.Rcode != dns.RcodeSuccess {
-		return nil
+		return ips
 	}
 	// Stuff must be in the answer section
 	for _, a := range in.Answer {
-		ips = append(ips, a.(*dns.RR_AAAA).AAAA)
+		ips = append(ips, a.(*dns.RR_AAAA).AAAA.String()+":53")
 	}
 	return ips
 }
