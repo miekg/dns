0ffb28d3d99bfc1af62edb556567f593b42e71de
Miek Gieben <miek@miek.nl>
2011-09-11 21:34:42 +0200

    start adding support for tsig in axfr

--- _examples/axfr/axfr.go
+++ _examples/axfr/axfr.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	var serial *int = flag.Int("serial", 0, "Perform an IXFR with the given serial")
 	var nameserver *string = flag.String("ns", "127.0.0.1:53", "Query this nameserver")
+	var secret *string = flag.String("secret", "", "Use this secret for TSIG")
 	flag.Parse()
 	zone := flag.Arg(flag.NArg() - 1)
 
