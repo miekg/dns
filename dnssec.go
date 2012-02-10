448596bccbe078a0bab8922dd16d122cd8a8bb9f
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-02-10 09:19:23 +0100

    fix typo

--- dnssec.go
+++ dnssec.go
@@ -280,7 +280,7 @@ func (s *RR_RRSIG) Sign(k PrivateKey, rrset []RR) error {
 // cryptographic test, the signature validity period must be checked separately.
 // This function (temporary) modifies the RR for the validation to work. 
 func (s *RR_RRSIG) Verify(k *RR_DNSKEY, rrset []RR) error {
-	// Frist the easy checks
+	// First the easy checks
 	if s.KeyTag != k.KeyTag() {
 		return ErrKey
 	}
