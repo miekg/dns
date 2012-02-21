bad984954b6b4ddb431fc207a8b1e4069722365c
Miek Gieben <miek@miek.nl>
2012-02-21 22:43:24 +0100

    go fmt and line nr tweak

--- scanner.go
+++ scanner.go
@@ -1,4 +1,5 @@
 package dns
+
 // Implement a simple scanner, return a byte stream from an io reader.
 
 import (
@@ -8,26 +9,27 @@ import (
 )
 
 type scan struct {
-	src *bufio.Reader
-        position scanner.Position
+	src      *bufio.Reader
+	position scanner.Position
 }
 
 func scanInit(r io.Reader) *scan {
 	s := new(scan)
 	s.src = bufio.NewReader(r)
+	s.position.Line = 1
 	return s
 }
 
 // tokenText returns the next byte from the input
 func (s *scan) tokenText() (byte, error) {
-        c, err := s.src.ReadByte()
-        if err != nil {
-                return c, err
-        }
-        if c == '\n' {
-                s.position.Line++
-                s.position.Column = 0
-        }
-        s.position.Column++
-        return c, nil
+	c, err := s.src.ReadByte()
+	if err != nil {
+		return c, err
+	}
+	if c == '\n' {
+		s.position.Line++
+		s.position.Column = 0
+	}
+	s.position.Column++
+	return c, nil
 }
