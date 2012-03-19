33a58c8cb7ae71b44b1009a5542d0b3adfb42e76
Miek Gieben <miek@miek.nl>
2012-03-19 20:00:40 +0100

    make it compile

--- dnssec.go
+++ dnssec.go
@@ -147,14 +147,14 @@ func (k *RR_DNSKEY) ToDS(h int) *RR_DS {
 	wire := make([]byte, DefaultMsgSize)
 	n, ok := packStruct(keywire, wire, 0)
 	if !ok {
-		return ErrPack
+		return nil
 	}
 	wire = wire[:n]
 
 	owner := make([]byte, 255)
 	off, ok1 := PackDomainName(k.Hdr.Name, owner, 0, nil, false)
 	if !ok1 {
-		return ErrPack
+		return nil
 	}
 	owner = owner[:off]
 	// RFC4034:
