025d0cecf44b431a61a4308c0bed10533805f481
Miek Gieben <miek@miek.nl>
2012-05-31 21:40:49 +0200

    Fix the test: EDNS0 is done completely different

--- dns_test.go
+++ dns_test.go
@@ -67,18 +67,6 @@ func TestPackUnpack2(t *testing.T) {
 	}
 }
 
-func TestEDNS_RR(t *testing.T) {
-	edns := new(RR_OPT)
-	edns.Hdr.Name = "." // must . be for edns
-	edns.Hdr.Rrtype = TypeOPT
-	edns.Hdr.Class = ClassINET
-	edns.Hdr.Ttl = 3600
-	edns.Option = make([]Option, 1)
-	edns.Option[0].Code = OptionCodeNSID
-	edns.Option[0].Data = "lalalala"
-	//t..Logf("%v\n", edns)
-}
-
 func TestBailiwick(t *testing.T) {
 	yes := map[string]string{
 		"miek.nl": "ns.miek.nl",
