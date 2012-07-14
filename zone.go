68f08416ff6827ce80ae067b968df5b81b75a826
Miek Gieben <miek@miek.nl>
2012-07-14 22:54:49 +0200

    more stuff

--- zone.go
+++ zone.go
@@ -16,5 +16,22 @@ type ZoneData struct {
 	RR         map[uint16][]RR        // Map ...
 	Signatures map[uint16][]*RR_RRSIG // DNSSEC signatures
 	Glue       bool                   // True if the A and AAAA record are glue
-	// nsec3, next
+}
+
+func lessZone(a, b interface{}) bool { return a.(string) < b.(string) }
+
+// New ...
+func New(name string) *Zone {
+	z := new(Zone)
+	z.Name = name
+	z.Tree = llrb.New(lessZone)
+	return z
+}
+
+func (z *Zone) Insert(r RR) {
+	zd := z.Tree.Get(r.Header().Name)
+}
+
+func (z *Zone) Remove(r RR) {
+
 }
