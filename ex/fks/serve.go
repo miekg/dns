051ceaa4546806618b0560ed560666cd044f3e08
Miek Gieben <miek@miek.nl>
2012-08-03 09:48:47 -0700

    more stuff for a nameserver

--- ex/fks/serve.go
+++ ex/fks/serve.go
@@ -7,12 +7,10 @@ import (
 
 func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 	// See RFC 1035...
+		m := new(dns.Msg)
+		m.SetRcode(req, dns.RcodeNameError)
 
 	log.Printf("incoming %s\n", req.Question[0].Name)
-	// dynamic updates
-	m := new(dns.Msg)
-	m.SetRcode(req, dns.RcodeNameError)
-
 	// For now:
 	// look up name -> yes, continue, no -> nxdomain
 	node := z.Find(req.Question[0].Name)
@@ -23,5 +21,15 @@ func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 		w.Write(m)
 		return
 	}
+	// Referral?
+	// Name found, look for type, yes, answer, no 
+	if rrs, ok := node.RR[req.Question[0].Qtype] {
+		// rrs match name and type
+		// Need to look at class to but... no
+		// create answer
+	}
+
+
+
 	w.Write(m)
 }
