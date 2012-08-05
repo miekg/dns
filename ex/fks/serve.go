6360aca9d2963463f86be27fa7d818d3e49e0de4
Miek Gieben <miek@miek.nl>
2012-08-04 19:29:29 -0700

    fix erros

--- ex/fks/serve.go
+++ ex/fks/serve.go
@@ -6,14 +6,13 @@ import (
 )
 
 func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
-	log.Printf("incoming %s %s\n", req.Question[0].Name, dns.Rr_str[req.Question[0].Qtype])
+	log.Printf("incoming %s %s %d\n", req.Question[0].Name, dns.Rr_str[req.Question[0].Qtype], req.MsgHdr.Id)
 	// Referral
 	// if we find something with NonAuth = true, it means
 	// we need to return referral
 	nss := z.Predecessor(req.Question[0].Name)
+	m := new(dns.Msg)
 	if nss != nil && nss.NonAuth {
-		log.Printf("Referral")
-		m := new(dns.Msg)
 		m.SetReply(req)
 		m.Ns = nss.RR[dns.TypeNS]
 		// lookup the a records for additional, only when
@@ -26,7 +25,6 @@ func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 	// If we don't have the name return NXDOMAIN
 	node := z.Find(req.Question[0].Name)
 	if node == nil {
-		m := new(dns.Msg)
 		m.SetRcode(req, dns.RcodeNameError)
 		w.Write(m)
 		return
@@ -36,8 +34,6 @@ func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 	// we still have NSs for this name. If we have nss and they
 	// are NonAuth true return those.
 	if nss, ok := node.RR[dns.TypeNS]; ok && node.NonAuth {
-		log.Printf("Referral")
-		m := new(dns.Msg)
 		m.SetReply(req)
 		m.Ns = nss
 		// lookup the a records for additional, only when
@@ -49,7 +45,6 @@ func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 	apex := z.Find(z.Origin)
 
 	if rrs, ok := node.RR[req.Question[0].Qtype]; ok {
-		m := new(dns.Msg)
 		m.SetReply(req)
 		m.MsgHdr.Authoritative = true
 		m.Answer = rrs
@@ -57,13 +52,11 @@ func serve(w dns.ResponseWriter, req *dns.Msg, z *dns.Zone) {
 		w.Write(m)
 		return
 	} else { // NoData reply
-		m := new(dns.Msg)
 		m.SetReply(req)
 		m.Ns = apex.RR[dns.TypeSOA]
 		w.Write(m)
 		return
 	}
-	m := new(dns.Msg)
 	m.SetRcode(req, dns.RcodeNameError)
 	w.Write(m)
 }
--- ex/fks/zones.go
+++ ex/fks/zones.go
@@ -11,16 +11,17 @@ func addZone(zones map[string]*dns.Zone, origin, file string) error {
 	origin = dns.Fqdn(origin)
 	z1 := dns.NewZone(origin)
 	if z1 == nil {
-		return errors.New("boe")
+		return errors.New("fks: failed to open %s", file)
 	}
 	f, e := os.Open(file)
 	if e != nil {
 		return e
 	}
 	for rr := range dns.ParseZone(f, origin, file) {
-		// TODO(mg): blab something about the error?
 		if rr.Error == nil {
 			z1.Insert(rr.RR)
+		} else {
+			log.Printf("fks: failed to parse: %s\n", rr.Error.Error())
 		}
 	}
 	zones[origin] = z1
