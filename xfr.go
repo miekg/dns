d98cc3f2f73923284ef6958d5ab61c5579984c58
Miek Gieben <miek@miek.nl>
2012-08-28 08:14:12 +0200

    rename XfrMsg to XfrToken

--- xfr.go
+++ xfr.go
@@ -1,21 +1,13 @@
 package dns
 
-import (
-	"time"
-)
-
-// TODO: axfrreceive fixen, first can go
-// a tsigTimersonly to responsewriter
-
-// XfrMsg is used when doing [IA]xfr with a remote server.
-type XfrMsg struct {
+// XfrToken is used when doing [IA]xfr with a remote server.
+type XfrToken struct {
 	RR    []RR          // the set of RRs in the answer section form the message of the server
-	Rtt   time.Duration // round trip time  
 	Error error         // if something went wrong, this contains the error  
 }
 
 // XfrReceive performs a [AI]xfr request (depends on the message's Qtype). It returns
-// a channel of XfrMsg on which the replies from the server are sent. At the end of
+// a channel of XfrToken on which the replies from the server are sent. At the end of
 // the transfer the channel is closed.
 // It panics if the Qtype does not equal TypeAXFR or TypeIXFR. The messages are TSIG checked if
 // needed, no other post-processing is performed. The caller must dissect the returned
@@ -28,7 +20,7 @@ type XfrMsg struct {
 //	for r := range t {
 //		// ... deal with r.RR or r.Error
 //	}
-func (c *Client) XfrReceive(q *Msg, a string) (chan *XfrMsg, error) {
+func (c *Client) XfrReceive(q *Msg, a string) (chan *XfrToken, error) {
 	w := new(reply)
 	w.client = c
 	w.addr = a
@@ -39,7 +31,7 @@ func (c *Client) XfrReceive(q *Msg, a string) (chan *XfrMsg, error) {
 	if err := w.send(q); err != nil {
 		return nil, err
 	}
-	e := make(chan *XfrMsg)
+	e := make(chan *XfrToken)
 	switch q.Question[0].Qtype {
 	case TypeAXFR:
 		go w.axfrReceive(e)
@@ -53,23 +45,23 @@ func (c *Client) XfrReceive(q *Msg, a string) (chan *XfrMsg, error) {
 	panic("not reached")
 }
 
-func (w *reply) axfrReceive(c chan *XfrMsg) {
+func (w *reply) axfrReceive(c chan *XfrToken) {
 	first := true
 	defer w.conn.Close()
 	defer close(c)
 	for {
 		in, err := w.receive()
 		if err != nil {
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: err}
+			c <- &XfrToken{in.Answer, err}
 			return
 		}
 		if w.req.Id != in.Id {
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: ErrId}
+			c <- &XfrToken{in.Answer, ErrId}
 			return
 		}
 		if first {
 			if !checkXfrSOA(in, true) {
-				c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: ErrXfrSoa}
+				c <- &XfrToken{in.Answer, ErrXfrSoa}
 				return
 			}
 			first = !first
@@ -78,16 +70,16 @@ func (w *reply) axfrReceive(c chan *XfrMsg) {
 		if !first {
 			w.tsigTimersOnly = true // Subsequent envelopes use this.
 			if checkXfrSOA(in, false) {
-				c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: nil}
+				c <- &XfrToken{in.Answer, nil}
 				return
 			}
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: nil}
+			c <- &XfrToken{in.Answer, nil}
 		}
 	}
 	panic("not reached")
 }
 
-func (w *reply) ixfrReceive(c chan *XfrMsg) {
+func (w *reply) ixfrReceive(c chan *XfrToken) {
 	var serial uint32 // The first serial seen is the current server serial
 	first := true
 	defer w.conn.Close()
@@ -95,23 +87,23 @@ func (w *reply) ixfrReceive(c chan *XfrMsg) {
 	for {
 		in, err := w.receive()
 		if err != nil {
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: err}
+			c <- &XfrToken{in.Answer, err}
 			return
 		}
 		if w.req.Id != in.Id {
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: ErrId}
+			c <- &XfrToken{in.Answer, ErrId}
 			return
 		}
 		if first {
 			// A single SOA RR signals "no changes"
 			if len(in.Answer) == 1 && checkXfrSOA(in, true) {
-				c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: nil}
+				c <- &XfrToken{in.Answer, nil}
 				return
 			}
 
 			// Check if the returned answer is ok
 			if !checkXfrSOA(in, true) {
-				c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: ErrXfrSoa}
+				c <- &XfrToken{in.Answer, ErrXfrSoa}
 				return
 			}
 			// This serial is important
@@ -125,47 +117,14 @@ func (w *reply) ixfrReceive(c chan *XfrMsg) {
 			// If the last record in the IXFR contains the servers' SOA,  we should quit
 			if v, ok := in.Answer[len(in.Answer)-1].(*RR_SOA); ok {
 				if v.Serial == serial {
-					c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt, Error: nil}
+					c <- &XfrToken{in.Answer, nil}
 					return
 				}
 			}
-			c <- &XfrMsg{RR: in.Answer, Rtt: w.rtt}
+			c <- &XfrToken{in.Answer, nil}
 		}
 	}
 	panic("not reached")
-}
-
-// XfrSend performs an outgoing [IX]xfr depending on the request message. As
-// long as the channel c is open ... TODO(mg): docs
-// tsig is done, enveloping is done, voor de rest niks... TODO
-func XfrSend(w ResponseWriter, req *Msg, c chan *XfrMsg) error {
-	switch req.Question[0].Qtype {
-	case TypeAXFR, TypeIXFR:
-		go axfrSend(w, req, c)
-	default:
-		return ErrXfrType
-	}
-	return nil
-}
-
-// TODO(mg): count the RRs and the resulting size.
-func axfrSend(w ResponseWriter, req *Msg, c chan *XfrMsg) {
-	rep := new(Msg)
-	rep.SetReply(req)
-	rep.MsgHdr.Authoritative = true
-
-	first := true
-	w.TsigTimersOnly(false)
-	for x := range c {
-		// assume is fits
-		rep.Answer = append(rep.Answer, x.RR...)
-		w.Write(rep)
-		if first {
-			first = !first
-			w.TsigTimersOnly(first)
-		}
-		rep.Answer = nil
-	}
 }
 
 // Check if he SOA record exists in the Answer section of 
@@ -181,3 +140,37 @@ func checkXfrSOA(in *Msg, first bool) bool {
 	}
 	return false
 }
+
+// XfrSend performs an outgoing [IX]xfr depending on the request message. As
+// long as the channel c is open ... TODO(mg): docs
+// tsig is done, enveloping is done, voor de rest niks... TODO
+func XfrSend(w ResponseWriter, req *Msg, c chan *XfrToken) error {
+	switch req.Question[0].Qtype {
+	case TypeAXFR, TypeIXFR:
+		go axfrSend(w, req, c)
+	default:
+		return ErrXfrType
+	}
+	return nil
+}
+
+// TODO(mg): count the RRs and the resulting size.
+func axfrSend(w ResponseWriter, req *Msg, c chan *XfrToken) {
+	rep := new(Msg)
+	rep.SetReply(req)
+	rep.MsgHdr.Authoritative = true
+
+	first := true
+	w.TsigTimersOnly(false)
+	for x := range c {
+		// assume it fits
+		rep.Answer = append(rep.Answer, x.RR...)
+		w.Write(rep)
+		if first {
+			first = !first
+			w.TsigTimersOnly(first)
+		}
+		rep.Answer = nil
+	}
+}
+
