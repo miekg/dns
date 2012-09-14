7b351c0f086fbf6493e034a6ed462f29d26452fd
Miek Gieben <miek@miek.nl>
2012-09-14 09:48:52 +0200

    added

---
+++ tlsa.go
@@ -0,0 +1,13 @@
+package dns
+
+// TLSA support functions
+
+// Sign creates a TLSA record from a SSL certifcate.
+func (t *RR_TLSA) Sign() {
+
+}
+
+// Verify verifies a TLSA record against a SSL certificate.
+func (t *RR_TLSA) Verify() {
+
+}
