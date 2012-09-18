1b7b7d6f742b26eba6daf121ea8dffcdd688f8f4
Miek Gieben <miek@miek.nl>
2012-09-18 08:19:06 +0200

    add more testcases

--- server_test.go
+++ server_test.go
@@ -76,13 +76,23 @@ func TestDotAsCatchAllWildcard(t *testing.T) {
 	mux.Handle(".", HandlerFunc(HelloServer))
 	mux.Handle("example.com.", HandlerFunc(AnotherHelloServer))
 
-	handler := mux.match("www.mike.nl", TypeTXT)
+	handler := mux.match("www.miek.nl.", TypeTXT)
 	if handler == nil {
 		t.Error("wildcard match failed")
 	}
 
-	nhandler := mux.match("www.example.com", TypeTXT)
-	if nhandler == nil {
-		t.Error("match failed")
+	handler = mux.match("www.example.com.", TypeTXT)
+	if handler == nil {
+		t.Error("example.com match failed")
+	}
+
+	handler = mux.match("a.www.example.com.", TypeTXT)
+	if handler == nil {
+		t.Error("a.www.example.com match failed")
+	}
+
+	handler = mux.match("boe.", TypeTXT)
+	if handler == nil {
+		t.Error("boe. match failed")
 	}
 }
