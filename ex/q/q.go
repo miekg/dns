0069ce218a31c38e1e45e90ede6afe25a6c783d7
Miek Gieben <miek@miek.nl>
2012-10-14 11:30:08 +0200

    docs

--- ex/q/q.go
+++ ex/q/q.go
@@ -1,3 +1,7 @@
+// Q is a small utility which acts and behaves like 'dig' from BIND.
+// It is meant to stay lean and mean, while having a bunch of handy
+// features, like -check which checks if a packet is correctly signed (without
+// checking the chain of trust).
 package main
 
 import (
