553b780257759f6920b40bff9591ad342fbba6d6
Miek Gieben <miek@miek.nl>
2012-11-18 13:28:16 +0100

    Rearrange some comments

--- client.go
+++ client.go
@@ -9,10 +9,8 @@ import (
 )
 
 // Order of events:
-// *client -> *reply -> Exchange*() -> dial()/send()->write()/receive()->read()
-
+// *client -> *reply -> Exchange() -> dial()/send()->write()/receive()->read()
 // Do I want make this an interface thingy?
-
 type reply struct {
 	client         *Client
 	addr           string
