2a346164195c865bb3cbfa72cf16e25ee1630634
Miek Gieben <miek@miek.nl>
2013-01-25 08:38:48 +0100

    And gone

--- marshal.go
+++
@@ -1,9 +0,0 @@
-package dns
-
-// UnmarshalJSON(data []byte) (err error)
-func (rr *A) MarshalJSON() ([]byte, error) {
-//	b, _ := json.Marshal(rr.Header())
-b := []byte(`"` + rr.A.String() + `"`)
-	return b, nil
-}
-
