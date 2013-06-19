667f7eb216cfa99f99effff1040cf73ffe5fcc59
Miek Gieben <miek@miek.nl>
2013-06-19 15:22:55 +0000

    Added

---
+++ ex/as212/as212.go
@@ -0,0 +1,8 @@
+// Copyright 2011 Miek Gieben. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// An AS212 blackhole DNS server. Similar to the one found in evldns.
+// Also see https://www.as112.net/
+
+
