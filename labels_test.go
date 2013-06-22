76db7d1a62405c9700bd76a93a1601ef3564cd49
Miek Gieben <miek@miek.nl>
2013-06-22 09:40:00 +0100

    add benchmarking

--- labels_test.go
+++ labels_test.go
@@ -98,3 +98,11 @@ func BenchmarkCompareLabels(b *testing.B) {
 		CompareDomainName("www.example.com", "aa.example.com")
 	}
 }
+
+func BenchmarkIsSubDomain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsSubDomain("www.example.com", "aa.example.com")
+		IsSubDomain("example.com", "aa.example.com")
+		IsSubDomain("miek.nl", "aa.example.com")
+	}
+}
