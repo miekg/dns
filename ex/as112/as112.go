a6268506fd9129acbd20e5d3c81d22e6821dfd31
Miek Gieben <miek@miek.nl>
2013-06-23 10:02:32 +0100

    correct the name

--- ex/as112/as112.go
+++ ex/as112/as112.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// An AS212 blackhole DNS server. Similar to the one found in evldns.
+// An AS112 blackhole DNS server. Similar to the one found in evldns.
 // Also see https://www.as112.net/
 
 package main
