ca87093b6d0b1cb41d55df82e8143c4dab0c7dee
Miek Gieben <miek@miek.nl>
2014-01-28 22:43:37 +0000

    Revert "Add benchmark for message unpacking"
    This reverts commit dc4eddaf9acd873ba8efd68e467cc9286f4591af.

--- dns_test.go
+++ dns_test.go
@@ -302,26 +302,6 @@ func BenchmarkMsgPackBuffer(b *testing.B) {
 	}
 }
 
-func BenchmarkMsgUnpack(b *testing.B) {
-	makeMsg := func(question string, ans, ns, e []RR) *Msg {
-		msg := new(Msg)
-		msg.SetQuestion(Fqdn(question), TypeANY)
-		msg.Answer = append(msg.Answer, ans...)
-		msg.Ns = append(msg.Ns, ns...)
-		msg.Extra = append(msg.Extra, e...)
-		msg.Compress = true
-		return msg
-	}
-	name1 := "12345678901234567890123456789012345.12345678.123."
-	rrMx, _ := NewRR(name1 + " 3600 IN MX 10 " + name1)
-	msg := makeMsg(name1, []RR{rrMx, rrMx}, nil, nil)
-	msg_buf, _ := msg.Pack()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = msg.Unpack(msg_buf)
-	}
-}
-
 func BenchmarkPackDomainName(b *testing.B) {
 	name1 := "12345678901234567890123456789012345.12345678.123."
 	buf := make([]byte, len(name1)+1)
