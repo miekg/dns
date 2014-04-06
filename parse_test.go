8f8b6751034831e6e6de8587c296e7866ae5e7dc
Miek Gieben <miek@miek.nl>
2014-04-06 09:29:42 +0100

    Add better parsing test

--- parse_test.go
+++ parse_test.go
@@ -1117,12 +1117,17 @@ func TestTxtLong(t *testing.T) {
 
 func TestMalformedPackets(t *testing.T) {
 	var packets = []string{
-		"0021641c000000010000000000000b757361706f6f6c70726f7303636f6d0000100001",
+		"0021641c0000000100000000000078787878787878787878787303636f6d0000100001",
 	}
 
+// com = 63 6f 6d
 	for _, packet := range packets {
 		data, _ := hex.DecodeString(packet)
+//		for _, v := range data {
+//			t.Logf("%s ", string(v))
+//		}
 		var msg Msg
 		msg.Unpack(data)
+//		println(msg.String())
 	}
 }
