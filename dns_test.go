5a8fe5d5f72be6efcbd5ba0f4147e9da17cb0778
Miek Gieben <miek@miek.nl>
2014-06-06 21:10:38 +0100

    Add Copy test

--- dns_test.go
+++ dns_test.go
@@ -479,3 +479,11 @@ func TestRdataOverflow(t *testing.T) {
 		t.Fatalf("Oversize rrdata pack didn't return ErrRdata - instead: %v", err)
 	}
 }
+
+func TestCopy(t *testing.T) {
+	rr, _ := NewRR("miek.nl. 2311 IN A 127.0.0.1") // Weird TTL to avoid catching TTL
+	rr1 := Copy(rr)
+	if rr.String() != rr1.String() {
+		t.Fatalf("Copy() failed %s != %s", rr.String(), rr1.String())
+	}
+}
