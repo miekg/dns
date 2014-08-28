d1ea8497adf63e8a5b5b83ced2afe04a467d07b9
Alex Sergeyev <asergeyev@dyn.com>
2014-08-28 08:11:18 -0400

    Added couple failure tests for lowercase class/type

--- parse_test.go
+++ parse_test.go
@@ -514,6 +514,8 @@ func TestParseFailure(t *testing.T) {
 		"miek.nl. IN AAAA ::x",
 		"miek.nl. IN MX a0 miek.nl.",
 		"miek.nl aap IN MX mx.miek.nl.",
+		"miek.nl 200 IN mxx 10 mx.miek.nl.",
+		"miek.nl. inn MX 10 mx.miek.nl.",
 		// "miek.nl. IN CNAME ", // actually valid nowadays, zero size rdata
 		"miek.nl. IN CNAME ..",
 		"miek.nl. PA MX 10 miek.nl.",
