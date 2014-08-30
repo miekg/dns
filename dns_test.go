06fc321c2c31f0abb152a9ad2c134f8c0d88230d
Miek Gieben <miek@miek.nl>
2014-08-30 08:45:10 +0100

    fmt

--- dns_test.go
+++ dns_test.go
@@ -319,13 +319,13 @@ func TestMsgLength2(t *testing.T) {
 func TestMsgLengthCompressionMalformed(t *testing.T) {
 	// SOA with empty hostmaster, which is illegal
 	soa := &SOA{Hdr: RR_Header{Name: ".", Rrtype: TypeSOA, Class: ClassINET, Ttl: 12345},
-        Ns:      ".",
-        Mbox:    "",
-        Serial:  0,
-        Refresh: 28800,
-        Retry:   7200,
-        Expire:  604800,
-        Minttl:  60}
+		Ns:      ".",
+		Mbox:    "",
+		Serial:  0,
+		Refresh: 28800,
+		Retry:   7200,
+		Expire:  604800,
+		Minttl:  60}
 	m := new(Msg)
 	m.Compress = true
 	m.Ns = []RR{soa}
