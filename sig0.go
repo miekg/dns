241f9e6ce54b0e20aaa302bdd067925da911da9c
Miek Gieben <miek@miek.nl>
2014-11-03 08:04:16 +0000

    There is a copyright file now

--- sig0.go
+++ sig0.go
@@ -1,7 +1,3 @@
-// Copyright 2011 Miek Gieben. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 // SIG(0)
 //
 // From RFC 2931:
