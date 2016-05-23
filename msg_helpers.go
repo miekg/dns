c5317efbe93ba328c083b50d163acdf56512e0d4
Miek Gieben <miek@miek.nl>
2016-05-23 22:17:58 +0100

    add a and aaaa functions

--- msg_helpers.go
+++ msg_helpers.go
@@ -8,24 +8,24 @@ import (
 
 // helper functions called from the generated zmsg.go
 
-// These function are named after the tag the help pack/unpack, if there is no tag it is the name
-// of the type they pack/unpack (string, int, etc). We prefix all with unpack or pack, so packA or
-// packDomainName.
+// These function are named after the tag to help pack/unpack, if there is no tag it is the name
+// of the type they pack/unpack (string, int, etc). We prefix all with unpackData or packData, so packDataA or
+// packDataDomainName.
 
-func unpackA(msg []byte, off int) (a net.IP, off1 int, err error) {
+func unpackDataA(msg []byte, off int) (net.IP, int, error) {
 	lenmsg := len(msg)
-	if off == lenmsg {
-		return nil, off, nil // dyn. update
+	if dynamicUpdate(off, lenmsg) {
+		return nil, off, nil
 	}
 	if off+net.IPv4len > lenmsg {
 		return nil, lenmsg, &Error{err: "overflow unpacking a"}
 	}
-	a = net.IPv4(msg[off], msg[off+1], msg[off+2], msg[off+3])
+	a := net.IPv4(msg[off], msg[off+1], msg[off+2], msg[off+3])
 	off += net.IPv4len
 	return a, off, nil
 }
 
-func packA(a net.IP, msg []byte, off int) (off1 int, err error) {
+func packDataA(a net.IP, msg []byte, off int) (int, error) {
 	lenmsg := len(msg)
 	// It must be a slice of 4, even if it is 16, we encode only the first 4
 	if off+net.IPv4len > lenmsg {
@@ -52,6 +52,35 @@ func packA(a net.IP, msg []byte, off int) (off1 int, err error) {
 	return off, nil
 }
 
+func unpackDataAAAA(msg []byte, off int) (net.IP, int, error) {
+	lenmsg := len(msg)
+	if dynamicUpdate(off, lenmsg) {
+		return nil, off, nil
+	}
+	if off+net.IPv6len > lenmsg {
+		return nil, lenmsg, &Error{err: "overflow unpacking aaaa"}
+	}
+	aaaa := net.IP{msg[off], msg[off+1], msg[off+2], msg[off+3], msg[off+4],
+		msg[off+5], msg[off+6], msg[off+7], msg[off+8], msg[off+9], msg[off+10],
+		msg[off+11], msg[off+12], msg[off+13], msg[off+14], msg[off+15]}
+	off += net.IPv6len
+	return aaaa, off, nil
+}
+
+func packDataAAAA(aaaa net.IP, msg []byte, off int) (int, error) {
+	lenmsg := len(msg)
+	laaaa := len(aaaa)
+
+	if laaaa > net.IPv6len || off+laaaa > lenmsg {
+		return lenmsg, &Error{err: "overflow packing aaaa"}
+	}
+	for i := 0; i < net.IPv6len; i++ {
+		msg[off] = aaaa[i]
+		off++
+	}
+	return off, nil
+}
+
 // helper helper functions.
 
 func fromBase32(s []byte) (buf []byte, err error) {
@@ -73,6 +102,10 @@ func fromBase64(s []byte) (buf []byte, err error) {
 func unpackUint16(msg []byte, off int) (uint16, int) {
 	return uint16(msg[off])<<8 | uint16(msg[off+1]), off + 2
 }
+
 func packUint16(i uint16) (byte, byte) { return byte(i >> 8), byte(i) }
 func toBase32(b []byte) string         { return base32.HexEncoding.EncodeToString(b) }
 func toBase64(b []byte) string         { return base64.StdEncoding.EncodeToString(b) }
+
+// dynamicUpdate returns true of off equals len.
+func dynamicUpdate(off, len int) bool { return off == len }
