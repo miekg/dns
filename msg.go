886b0ba0611800593fd432e8254ab40d689c6040
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-02-04 20:18:27 +1030

    Document lack of performance overhead from mutex in `func id()`

--- msg.go
+++ msg.go
@@ -80,8 +80,14 @@ func id() uint16 {
 		idRand = rand.New(rand.NewSource(seed))
 	}
 
-	// The call to Uint32 must be within the mutex lock
-	// because *rand.Rand is not safe for concurrent use.
+	// The call to idRand.Uint32 must be within the
+	// mutex lock because *rand.Rand is not safe for
+	// concurrent use.
+	//
+	// There is no added performance overhead to calling
+	// idRand.Uint32 inside a mutex lock over just
+	// calling rand.Uint32 as the global math/rand rng
+	// is internally protected by a sync.Mutex.
 	id := uint16(idRand.Uint32())
 
 	idLock.Unlock()
