c8783cd1a31921e7c52424f41bde43596ceaaacb
Andrew Tunnell-Jones <andrew@tj.id.au>
2017-03-21 21:11:16 +0000

    Allow mixed case in NSEC3 NextDomain

--- issue_test.go
+++ issue_test.go
@@ -2,7 +2,10 @@ package dns
 
 // Tests that solve that an specific issue.
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestTCPRtt(t *testing.T) {
 	m := new(Msg)
@@ -40,5 +43,26 @@ func TestNSEC3MissingSalt(t *testing.T) {
 	out := m.Answer[0].(*NSEC3).Salt
 	if in != out {
 		t.Fatalf("expected salts to match. packed: `%s`. returned: `%s`", in, out)
+	}
+}
+
+func TestNSEC3MixedNextDomain(t *testing.T) {
+	rr, err := NewRR("ji6neoaepv8b5o6k4ev33abha8ht9fgc.example. NSEC3 1 1 12 - k8udemvp1j2f7eg6jebps17vp3n8i58h")
+	if err != nil {
+		t.Fatalf("failed to parse example rr: %s", err)
+	}
+	m := new(Msg)
+	m.Answer = []RR{rr}
+	mb, err := m.Pack()
+	if err != nil {
+		t.Fatalf("expected to pack message. err: %s", err)
+	}
+	if err := m.Unpack(mb); err != nil {
+		t.Fatalf("expected to unpack message. err: %s", err)
+	}
+	in := strings.ToUpper(rr.(*NSEC3).NextDomain)
+	out := m.Answer[0].(*NSEC3).NextDomain
+	if in != out {
+		t.Fatalf("expected round trip to produce NextDomain `%s`, instead `%s`", in, out)
 	}
 }
