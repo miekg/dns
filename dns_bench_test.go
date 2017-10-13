5f64fb22f93203c2007d273799b9b5eed83cbb32
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-10-14 01:51:12 +1030

    Simplify compressed length code (#527)
    * Add BenchmarkMsgLengthNoCompression
    
    * Simplify compressedLen loops
    
    * Fix typo in compressionLenSlice

--- dns_bench_test.go
+++ dns_bench_test.go
@@ -25,6 +25,25 @@ func BenchmarkMsgLength(b *testing.B) {
 	}
 }
 
+func BenchmarkMsgLengthNoCompression(b *testing.B) {
+	b.StopTimer()
+	makeMsg := func(question string, ans, ns, e []RR) *Msg {
+		msg := new(Msg)
+		msg.SetQuestion(Fqdn(question), TypeANY)
+		msg.Answer = append(msg.Answer, ans...)
+		msg.Ns = append(msg.Ns, ns...)
+		msg.Extra = append(msg.Extra, e...)
+		return msg
+	}
+	name1 := "12345678901234567890123456789012345.12345678.123."
+	rrMx, _ := NewRR(name1 + " 3600 IN MX 10 " + name1)
+	msg := makeMsg(name1, []RR{rrMx, rrMx}, nil, nil)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		msg.Len()
+	}
+}
+
 func BenchmarkMsgLengthPack(b *testing.B) {
 	makeMsg := func(question string, ans, ns, e []RR) *Msg {
 		msg := new(Msg)
