072dd859a749c950ee46d1ca511b28a6ae9be22b
Miek Gieben <miek@miek.nl>
2017-11-03 13:59:02 +0000

    Its not this

--- server.go
+++ server.go
@@ -385,22 +385,22 @@ func (srv *Server) ActivateAndServe() error {
 	}
 	pConn := srv.PacketConn
 	l := srv.Listener
-	if pConn != nil {
-		if srv.UDPSize == 0 {
-			srv.UDPSize = MinMsgSize
-		}
-		// Check PacketConn interface's type is valid and value is not nil
-		if t, ok := pConn.(*net.UDPConn); ok && t != nil {
-			if e := setUDPSocketOptions(t); e != nil {
-				return e
-			}
-			srv.started = true
-			srv.lock.Unlock()
-			e := srv.serveUDP(t)
-			srv.lock.Lock() // to satisfy the defer at the top
+
+	if srv.UDPSize == 0 {
+		srv.UDPSize = MinMsgSize
+	}
+	// Check PacketConn interface's type is valid and value is not nil
+	if t, ok := pConn.(*net.UDPConn); ok && t != nil {
+		if e := setUDPSocketOptions(t); e != nil {
 			return e
 		}
+		srv.started = true
+		srv.lock.Unlock()
+		e := srv.serveUDP(t)
+		srv.lock.Lock() // to satisfy the defer at the top
+		return e
 	}
+
 	if l != nil {
 		srv.started = true
 		srv.lock.Unlock()
