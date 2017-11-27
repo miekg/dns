9fe96cd6bd5bc2a72620cfe8397fdf16ff081592
Marc Ende <me@e-beyond.de>
2017-11-27 10:13:08 +0000

    Cleaned up test

--- clientconfig_test.go
+++ clientconfig_test.go
@@ -20,43 +20,7 @@ domain somedomain.com
 nameserver 10.28.10.2
 nameserver 11.28.10.1` // <- NOTE: NO newline.
 
-const normalWithOutNdots string = `
-# Comment
-domain somedomain.com
-nameserver 10.28.10.2
-nameserver 11.28.10.1
-`
-const normalWithNdots0 string = `
-# Comment
-domain somedomain.com
-nameserver 10.28.10.2
-nameserver 11.28.10.1
-options ndots:0
-`
-const normalWithNdots15 string = `
-# Comment
-domain somedomain.com
-nameserver 10.28.10.2
-nameserver 11.28.10.1
-options ndots:15
-`
-const normalWithNdots16 string = `
-# Comment
-domain somedomain.com
-nameserver 10.28.10.2
-nameserver 11.28.10.1
-options ndots:16
-`
-
-const normalWithNdotsBelow0 string = `
-# Comment
-domain somedomain.com
-nameserver 10.28.10.2
-nameserver 11.28.10.1
-options ndots:-1
-`
-
-func testConfig(t *testing.T, data string, expectedNdots int) {
+func testConfig(t *testing.T, data string) {
 	cc, err := ClientConfigFromReader(strings.NewReader(data))
 	if err != nil {
 		t.Errorf("error parsing resolv.conf: %v", err)
@@ -71,18 +35,35 @@ func testConfig(t *testing.T, data string, expectedNdots int) {
 			t.Errorf("domain is unexpected: %v", cc.Search[0])
 		}
 	}
+}
+
+func TestNameserver(t *testing.T)          { testConfig(t, normal) }
+func TestMissingFinalNewLine(t *testing.T) { testConfig(t, missingNewline) }
+
+func testNdotsConfig(t *testing.T, data string, expectedNdots int) {
+	cc, err := ClientConfigFromReader(strings.NewReader(data))
+	if err != nil {
+		t.Errorf("error parsing resolv.conf: %v", err)
+	}
 	if cc.Ndots != expectedNdots {
 		t.Errorf("Ndots not properly parsed: (Expected: %d / Was: %d)", expectedNdots, cc.Ndots)
 	}
 }
 
-func TestNameserver(t *testing.T)          { testConfig(t, normal, 1) }
-func TestMissingFinalNewLine(t *testing.T) { testConfig(t, missingNewline, 1) }
-func TestWithOutNdots(t *testing.T)        { testConfig(t, normalWithOutNdots, 1) }
-func TestNdots0(t *testing.T)              { testConfig(t, normalWithNdots0, 0) }
-func TestNdots15(t *testing.T)             { testConfig(t, normalWithNdots15, 15) }
-func TestNdots16(t *testing.T)             { testConfig(t, normalWithNdots16, 15) }
-func TestNdotsBelow0(t *testing.T)         { testConfig(t, normalWithNdotsBelow0, 0) }
+func TestNdots(t *testing.T) {
+	var ndotsVariants map[string]int = make(map[string]int)
+	ndotsVariants["options ndots:0"] = 0
+	ndotsVariants["options ndots:1"] = 1
+	ndotsVariants["options ndots:15"] = 15
+	ndotsVariants["options ndots:16"] = 15
+	ndotsVariants["options ndots:-1"] = 0
+	ndotsVariants[""] = 1
+
+	for key := range ndotsVariants {
+		testNdotsConfig(t, key, ndotsVariants[key])
+	}
+
+}
 
 func TestReadFromFile(t *testing.T) {
 	tempDir, err := ioutil.TempDir("", "")
