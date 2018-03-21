0fa5c03ca0b4bb4eec5e151cac44aef56845f0c0
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-03-21 13:01:39 +1030

    Reorder serveDNS arguments
    This makes it consistent with the ordering of arguments to
    serveUDPPacket and serveTCPConn.

--- server.go
+++ server.go
@@ -544,7 +544,7 @@ func (srv *Server) serveTCPConn(h Handler, t net.Conn) {
 			// TODO(tmthrgd): handle error
 			break
 		}
-		srv.serveDNS(m, w, h)
+		srv.serveDNS(h, m, w)
 		if w.tcp == nil {
 			break // Close() was called
 		}
@@ -565,10 +565,10 @@ func (srv *Server) serveUDPPacket(h Handler, m []byte, u *net.UDPConn, s *Sessio
 	} else {
 		w.writer = w
 	}
-	srv.serveDNS(m, w, h)
+	srv.serveDNS(h, m, w)
 }
 
-func (srv *Server) serveDNS(m []byte, w *response, h Handler) {
+func (srv *Server) serveDNS(h Handler, m []byte, w *response) {
 	req := new(Msg)
 	err := req.Unpack(m)
 	if err != nil { // Send a FormatError back
