8ccae88257a8cec016f407e36517a0861e33dda8
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-05-30 00:09:02 +0930

    Require URLs for DOH addresses (#684)
    * Require URLs for DOH addresses
    
    * Move time.Now directly above http.Client.Do in DoH
    
    * Remove https scheme check from DOH
    
    Although the draft RFC explicitly requires that the scheme be https,
    this was deemed undesirable, so remove it.

--- client.go
+++ client.go
@@ -12,7 +12,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 )
@@ -216,25 +215,13 @@ func (c *Client) exchangeDOH(ctx context.Context, m *Msg, a string) (r *Msg, rtt
 		return nil, 0, err
 	}
 
-	// TODO(tmthrgd): Allow the path to be customised?
-	u := &url.URL{
-		Scheme: "https",
-		Host:   a,
-		Path:   "/.well-known/dns-query",
-	}
-	if u.Port() == "443" {
-		u.Host = u.Hostname()
-	}
-
-	req, err := http.NewRequest(http.MethodPost, u.String(), bytes.NewReader(p))
+	req, err := http.NewRequest(http.MethodPost, a, bytes.NewReader(p))
 	if err != nil {
 		return nil, 0, err
 	}
 
 	req.Header.Set("Content-Type", dohMimeType)
 	req.Header.Set("Accept", dohMimeType)
-
-	t := time.Now()
 
 	hc := http.DefaultClient
 	if c.HTTPClient != nil {
@@ -244,6 +231,8 @@ func (c *Client) exchangeDOH(ctx context.Context, m *Msg, a string) (r *Msg, rtt
 	if ctx != context.Background() && ctx != context.TODO() {
 		req = req.WithContext(ctx)
 	}
+
+	t := time.Now()
 
 	resp, err := hc.Do(req)
 	if err != nil {
