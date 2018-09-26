bb15ecf81f8ea9a472984e4f4d9b1638d88b1830
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-09-26 22:36:07 +0930

    Simplify appendByte

--- types.go
+++ types.go
@@ -513,13 +513,10 @@ func appendByte(s []byte, b byte) []byte {
 	var buf [3]byte
 	bufs := strconv.AppendInt(buf[:0], int64(b), 10)
 	s = append(s, '\\')
-	for i := 0; i < 3-len(bufs); i++ {
+	for i := len(bufs); i < 3; i++ {
 		s = append(s, '0')
 	}
-	for _, r := range bufs {
-		s = append(s, r)
-	}
-	return s
+	return append(s, bufs...)
 }
 
 func nextByte(b []byte, offset int) (byte, int) {
