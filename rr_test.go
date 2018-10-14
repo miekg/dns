46f8cd2211f31da5cec97546e77c201a772aeb65
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-10-14 20:07:22 +1030

    Panic in testRR if the error is non-nil

--- rr_test.go
+++ rr_test.go
@@ -1,7 +1,11 @@
 package dns
 
-// testRR returns the RR from string s. The error is thrown away.
+// testRR is a helper that wraps a call to NewRR and panics if the error is non-nil.
 func testRR(s string) RR {
-	r, _ := NewRR(s)
+	r, err := NewRR(s)
+	if err != nil {
+		panic(err)
+	}
+
 	return r
 }
