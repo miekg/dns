34d23c00e1b4a7a8f335d49d56e220a9376ae8f5
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-11-28 07:42:44 +1030

    Add bounds check comment to dddToByte

--- msg.go
+++ msg.go
@@ -556,12 +556,12 @@ func unpackTxt(msg []byte, off0 int) (ss []string, off int, err error) {
 func isDigit(b byte) bool { return b >= '0' && b <= '9' }
 
 func dddToByte(s []byte) byte {
-	_ = s[2]
+	_ = s[2] // bounds check hint to compiler; see golang.org/issue/14808
 	return byte((s[0]-'0')*100 + (s[1]-'0')*10 + (s[2] - '0'))
 }
 
 func dddStringToByte(s string) byte {
-	_ = s[2]
+	_ = s[2] // bounds check hint to compiler; see golang.org/issue/14808
 	return byte((s[0]-'0')*100 + (s[1]-'0')*10 + (s[2] - '0'))
 }
 
