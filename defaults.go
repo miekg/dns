d903bf9f093b5b3f50f6432b56f556ddd1e10fc9
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-12-01 19:36:33 +1030

    Correct comment in IsEdns0 in regards to RFC 6891

--- defaults.go
+++ defaults.go
@@ -145,10 +145,9 @@ func (dns *Msg) IsTsig() *TSIG {
 // record in the additional section will do. It returns the OPT record
 // found or nil.
 func (dns *Msg) IsEdns0() *OPT {
-	// EDNS0 is at the end of the additional section, start there.
-	// We might want to change this to *only* look at the last two
-	// records. So we see TSIG and/or OPT - this a slightly bigger
-	// change though.
+	// RFC 6891, Section 6.1.1 allows the OPT record to appear
+	// anywhere in the additional record section, but it's usually at
+	// the end so start there.
 	for i := len(dns.Extra) - 1; i >= 0; i-- {
 		if dns.Extra[i].Header().Rrtype == TypeOPT {
 			return dns.Extra[i].(*OPT)
