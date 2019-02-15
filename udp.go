c5da7a5e4896cb625a1b8e61988dced0032d0edb
Jarno Rajahalme <jarno@covalent.io>
2019-02-15 11:45:27 +0200

    udp: Add LocalAddr to SessionUDP interface
    Add a LocalAddr() function to the SessionUDP interface, with default
    implementation returning the address the server is listening on (no
    functional change). This allows specialized implementations to return
    the address the request was sent to when, e.g., the server is
    listening on the IPv6 unspecified address ([::]).
    
    Signed-off-by: Jarno Rajahalme <jarno@covalent.io>

--- udp.go
+++ udp.go
@@ -30,6 +30,8 @@ type SessionUDP interface {
 	ReadRequest(conn *net.UDPConn) ([]byte, error)
 	// RemoteAddr returns the remote address of the last read UDP request
 	RemoteAddr() net.Addr
+	// LocalAddr returns the local address of the last read UDP request
+	LocalAddr() net.Addr
 	// WriteResponse writes a response to the last read UDP request.
 	// The response is sent to the UDP address the request came from.
 	WriteResponse(b []byte) (int, error)
@@ -92,8 +94,11 @@ func (s *sessionUDP) Clear() SessionUDP {
 	return s
 }
 
-// RemoteAddr returns the remote network address.
+// RemoteAddr returns the remote network address for the current request.
 func (s *sessionUDP) RemoteAddr() net.Addr { return s.raddr }
+
+// LocalAddr returns the local network address for the current request.
+func (s *sessionUDP) LocalAddr() net.Addr { return s.conn.LocalAddr() }
 
 // ReadRequest reads a single request from the session and keeps the request context
 func (s *sessionUDP) ReadRequest(conn *net.UDPConn) ([]byte, error) {
