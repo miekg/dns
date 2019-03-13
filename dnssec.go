e969525f7be617f164c7bc9685baa67f510277fb
Tom Thorogood <me+github@tomthorogood.co.uk>
2019-03-13 10:33:22 +1030

    Use for range loop in DNSKEY.publicKeyRSA

--- dnssec.go
+++ dnssec.go
@@ -556,9 +556,9 @@ func (k *DNSKEY) publicKeyRSA() *rsa.PublicKey {
 	pubkey := new(rsa.PublicKey)
 
 	var expo uint64
-	for i := 0; i < int(explen); i++ {
+	for _, v := range keybuf[keyoff:modoff] {
 		expo <<= 8
-		expo |= uint64(keybuf[keyoff+i])
+		expo |= uint64(v)
 	}
 	if expo > 1<<31-1 {
 		// Larger exponent than supported by the crypto package.
