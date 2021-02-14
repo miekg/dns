83a0af525b17a141333c47cca20bc9e5fcbb2c10
Josh Soref <jsoref@users.noreply.github.com>
2021-02-14 14:23:28 -0500

    spelling: previous
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- labels_test.go
+++ labels_test.go
@@ -123,7 +123,7 @@ func TestPrevLabel(t *testing.T) {
 	for s, i := range prever {
 		x, ok := PrevLabel(s.string, s.int)
 		if i != x {
-			t.Errorf("label should be %d, got %d, %t: preving %d, %s", i, x, ok, s.int, s.string)
+			t.Errorf("label should be %d, got %d, %t: previous %d, %s", i, x, ok, s.int, s.string)
 		}
 	}
 }
