06539e9513936dde71a81c745d57349fd0c0bf31
Chris O'Haver <cohaver@infoblox.com>
2021-12-03 12:56:32 -0500

    refactor to remove else block
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- tsig.go
+++ tsig.go
@@ -163,16 +163,20 @@ func tsigGenerateProvider(m *Msg, provider TsigProvider, requestMAC string, time
 		return nil, "", err
 	}
 
+	buf, err := tsigBuffer(mbuf, rr, requestMAC, timersOnly)
+	if err != nil {
+		return nil, "", err
+	}
+
 	t := new(TSIG)
+	// Copy all TSIG fields except MAC, its size, and time signed which are filled when signing.
+	*t = *rr
+	t.TimeSigned = 0
+	t.MAC = ""
+	t.MACSize = 0
 
 	if rr.Error == RcodeSuccess || rr.Error == RcodeBadTime {
-		// Only compute response MAC for non-errors and bad time errors (RFC 8945 5.3.2)
-		buf, err := tsigBuffer(mbuf, rr, requestMAC, timersOnly)
-		if err != nil {
-			return nil, "", err
-		}
-		// Copy all TSIG fields except MAC and its size, which are filled using the computed digest.
-		*t = *rr
+		// Only sign TSIGs for non-errors and bad time errors (RFC 8945 5.3.2)
 		mac, err := provider.Generate(buf, rr)
 		if err != nil {
 			return nil, "", err
@@ -180,12 +184,6 @@ func tsigGenerateProvider(m *Msg, provider TsigProvider, requestMAC string, time
 		t.TimeSigned = rr.TimeSigned
 		t.MAC = hex.EncodeToString(mac)
 		t.MACSize = uint16(len(t.MAC) / 2) // Size is half!
-	} else {
-		// Copy all TSIG fields except MAC, its size and time signed, since we aren't signing.
-		*t = *rr
-		t.TimeSigned = 0
-		t.MAC = ""
-		t.MACSize = 0
 	}
 
 	tbuf := make([]byte, Len(t))
